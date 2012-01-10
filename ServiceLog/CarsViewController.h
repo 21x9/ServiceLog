07a3a6db396773b740bca212421ee873dfa25328
Mark Adams <mark.adams86@gmail.com>
2012-01-10 14:09:40 -0800

    Added CarsViewController class and wired it up to the storyboard. It is embedded in a navigation controller which serves as the initial view controller for the application.

---
+++ ServiceLog/CarsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CarsViewController.h
+//  ServiceLog
+//
+//  Created by Jennifer Clarke on 1/10/12.
+//  Copyright (c) 2012 Interstellar Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarsViewController : UIViewController
+
+@end
